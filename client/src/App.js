1f01206dc68732bbbd818de3c9eff908b1446651
justinbather <justinbather@hotmail.com>
2023-10-25 18:33:19 -0400

    adds fetching starter code from txt file and sets it to the monaco editor

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,33 @@ import logo from "./logo.svg";
 import "./App.css";
 import MonacoEditor from "react-monaco-editor";
 
+import data from "./test.txt";
+import { useEffect, useState } from "react";
+
 function App() {
+  const [starterCode, setStarterCode] = useState("");
+  const readFile = () => {
+    const code = fetch(data)
+      .then((d) => d.text())
+      .then((text) => {
+        setStarterCode(text);
+      });
+  };
+
+  useEffect(() => {
+    readFile();
+  }, []);
+
   return (
-    <>
+    <div className="main">
       <MonacoEditor
         width="800"
         height="600"
         language="javascript"
         theme="vs-dark"
+        value={starterCode}
       />
-    </>
+    </div>
   );
 }
 
