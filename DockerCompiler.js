79e51841f3fd1f8beffcd376065a2e26c32ba36e
justinbather <justinbather@hotmail.com>
2023-10-27 16:30:58 -0400

    clean up and add docs

--- DockerCompiler.js
+++ DockerCompiler.js
@@ -16,7 +16,7 @@ export default class DockerCompiler {
     this.prepare(success);
   }
 
-  //* Prepares the files to be ran
+  //* Prepares the files to be run
   //* Writes user's code to a new file in /temp to be copied into the docker instance
   prepare(success) {
     fs.writeFile(`./temp/${this.fileName}`, this.code, (err) => {
@@ -29,18 +29,15 @@ export default class DockerCompiler {
     });
   }
 
-  //* This gets executed by child process which runs the bash script docker.sh
-  //* Docker.sh creates a container with the user's code to be ran by providing the file extension and executing cmd below
   execute(success) {
-    //! Implement a timeout
-    //? In bash script?
     console.time();
     let start = performance.now();
 
     let testId = "1"; //! temp var for test id to be given to script
+
+    //* This command gets ran to start docker and compilation process
     const cmd = `./docker.sh ${this.fileName} ${this.executor} ${this.dockerImage} ${testId}`;
 
-    //runs cmd with timeout, waiting for a success for error file to be written by the container to then return data in callback
     exec(cmd); //! Async
 
     const timeout = 5;
@@ -52,29 +49,31 @@ export default class DockerCompiler {
 
       fs.readFile("./temp/success.txt", "utf-8", (err, data) => {
         if (err && numberIntervals < timeout) {
-          // console.log(err);
           //* No success.txt found, look for errors.txt
+
           fs.readFile("./temp/errors.txt", "utf-8", (err, data) => {
             if (err) {
               //* No error.txt found, keep going
               return;
             } else {
               //* Found error.txt, call the CB with error data read from file
-              success(data); //error file found, return error info
+              success(data);
 
               console.log("Found error file");
               exec("rm ./temp/errors.txt");
               console.log("error file deleted");
+
               let end = performance.now();
               console.log(`Request took ${end - start}ms`);
+
               clearInterval(timer);
-              // return;
             }
           });
 
           return;
         } else if (err && numberIntervals >= timeout) {
           //* Timeout is up, return timeout error
+
           console.log("Request timed out. Time limit exceeded");
           success("Compilation Timeout Occured: Timelimit exceeded.");
           clearInterval(timer);
@@ -84,39 +83,15 @@ export default class DockerCompiler {
           console.log("Found success file");
           exec("rm ./temp/success.txt");
           console.log("success file deleted");
+
           let end = performance.now();
           console.log(`Request took ${end - start}ms`);
+
           clearInterval(timer);
-          // return;
         }
       });
 
       console.log(`completed ${numberIntervals} interval`);
-    }, 1000); //1 seconds
-
-    // exec(cmd, (error, stdout, stderr) => {
-    //   let data = "";
-    //   let errorData = "";
-    //   let hints = ""; // In case there is no error or stdout we should make sure the user
-    //   // knows to call the function in the code
-
-    //   if (error) {
-    //     //! Log error here
-    //     console.log(error);
-    //   }
-
-    //   if (stderr) {
-    //     errorData += stderr;
-    //   }
-
-    //   if (stdout) {
-    //     data = stdout;
-    //   } else if (!error && !stderr) {
-    //     hints = "Make sure you call your function!";
-    //   }
-
-    //   success(data, errorData, hints);
-    //   console.timeEnd();
-    // });
+    }, 1000); //1 second intervals
   }
 }
--- index.js
+++ index.js
@@ -23,32 +23,6 @@ app.use((req, res, next) => {
   next();
 });
 
-//* Temporarily uses local string var with javascript to simulate the ability to use JSON.stringify
-//* Will need to look into converting to binary on the front end before sending
-app.post("/js-test", (req, res) => {
-  const data = req.body;
-  console.log(req);
-
-  let str = `console.log('hello world')
-  console.log('#2')
-  const test = () => {
-    
-    return 'testing function'
-  }
-  console.log(test())
-  `;
-  let inputJSON = JSON.stringify(str);
-  //   console.log(data);
-  try {
-    let strJSON = JSON.parse(inputJSON);
-    eval(strJSON); //* runs function and outputs all code correctly to stdout or returns stderr in response
-
-    return res.status(200).json({ success: "code ran successfully" });
-  } catch (e) {
-    return res.status(400).json({ success: false, error: String(e) });
-  }
-});
-
 app.post("/compile-test", (req, res) => {
   let start = console.time();
   const code = req.body.code;
