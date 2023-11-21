054d05d9c02463f53626461a4c258544eb6de92d
justinbather <justinbather@hotmail.com>
2023-11-21 14:01:07 -0500

    ec2 deploy

--- temp/is_palindrome.mjs
+++ temp/is_palindrome.mjs
@@ -3,8 +3,7 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    return x < 0 ? false : (x === +x.toString().split("").reverse().join(""));
-
+  return x < 0 ? false : (x === +x.toString().split("").reverse().join(""));
 };import input from './is_palindrome.input.mjs';
 input.forEach((input) => { 
   console.log(isPalindrome(input));});
